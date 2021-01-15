1c9b278cd6e1e1f5b379d5bdfecd8f1d154121fb
John Doe <kurbanof.atabek@gmail.com>
2021-01-15 20:58:17 +0600

    react-pizza-lesson-5

--- src/index.js
+++ src/index.js
@@ -11,3 +11,39 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// const aiza = (arr, length) => {
+//   const result = [];
+//   for (let i = 0; i < arr.length / length; i++) {
+//     const newArray = [];
+//     for (let j = 0; j < length; j++) {
+//       newArray.push(arr[length * i + j]);
+//     }
+
+//     const filteredArray = newArray.filter((item) => item !== undefined);
+//     result.push(filteredArray);
+//   }
+//   return result;
+// };
+
+// const uluk = (arr, n) => {
+//   const result = [];
+//   let tr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i % n == 0) {
+//       if (tr.length !== 0) result.push(tr);
+//       tr = [];
+//     }
+//     tr.push(arr[i]);
+//   }
+
+//   result.push(tr);
+
+//   return result;
+// };
+
+// console.log(uluk(['a', 'b', 'c', 'd', 'e', 'f', 'j', 'k', 'e'], 8));
+
+// const munarbek = arr => arr[0]
+
+// console.log(munarbek(['a', 'b', 'c', 'd', 'e', '2']))
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import PizzaItem from '../components/pizzaItem';
+import Item from '../components/item';
 import Sort from '../components/sort';
 import Categories from '../components/categories';
 
 const Home = () => {
   const [pizzas, setPizzas] = React.useState([]);
-
+  const [snacks, setSnacks] = React.useState([]);
   const [search, setSearch] = React.useState('');
 
   React.useEffect(() => {
     fetch('http://localhost:3000/db.json')
       .then((response) => response.json())
-      .then(({ pizzas }) => setPizzas(pizzas));
+      .then(({ pizzas, snacks }) => {
+        setSnacks(snacks);
+        setPizzas(pizzas);
+      });
   }, []);
 
-  const handleSearchValue = (event) => {
-    setSearch(event.target.value);
-  };
+  const handleSearchValue = (event) => setSearch(event.target.value);
 
   return (
     <div className='content'>
@@ -44,6 +46,34 @@ const Home = () => {
             })
             .map((item) => {
               return <PizzaItem {...item} />;
+            })}
+        </div>
+        <h2 className='content__title'>Закуски</h2>
+        <div className='content__items'>
+          {snacks
+            .filter((item) => {
+              if (search === '') {
+                return item;
+              } else if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return item;
+              }
+            })
+            .map((item) => {
+              return <Item {...item} />;
+            })}
+        </div>
+        <h2 className='content__title'>Десерты</h2>
+        <div className='content__items'>
+          {snacks
+            .filter((item) => {
+              if (search === '') {
+                return item;
+              } else if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return item;
+              }
+            })
+            .map((item) => {
+              return <Item {...item} />;
             })}
         </div>
       </div>
